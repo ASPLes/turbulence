f0852809e7256c6c8c9c0799779d71eb9f05c724
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2007-07-27 16:35:34 +0000

    af-arch: * [fix] Closing axl release (0.4.6).

---
+++ trunk/turbulence/web/update-web.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp turbulence.css index.html downloads.html turbulence.html aspl@www.aspl.es:www/turbulence
+scp images/*.png images/*.gif images/*.jpg aspl@www.aspl.es:www/turbulence/images/
+
