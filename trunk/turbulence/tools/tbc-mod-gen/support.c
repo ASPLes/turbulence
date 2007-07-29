5e63d5e5522ef916028ca90c67f5ccab1933d0d5
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2007-07-29 20:51:22 +0000

    turbulence: * [fix] web updates and license header checks.

--- trunk/turbulence/tools/tbc-mod-gen/support.c
+++ trunk/turbulence/tools/tbc-mod-gen/support.c
@@ -2,8 +2,8 @@
  *  Copyright (C) 2007 Advanced Software Production Line, S.L.
  *
  *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; version 2.1 of the
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; version 2.1 of the
  *  License.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -11,17 +11,10 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this program; if not, write to the Free
- *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  *  02111-1307 USA
- *  
- *  You may find a copy of the license under this software is released
- *  at COPYING file. This is LGPL software: you are wellcome to
- *  develop propietary applications using this library withtout any
- *  royalty or fee but returning back any change, improvement or
- *  addition in the form of source code, project image, documentation
- *  patches, etc.
  *
  *  For comercial support on build BEEP enabled solutions, supporting
  *  turbulence based solutions, etc, contact us:
@@ -33,7 +26,7 @@
  *         Spain
  *
  *      Email address:
- *         info@aspl.es - http://fact.aspl.es
+ *         info@aspl.es - http://www.turbulence.ws
  */
 #include <support.h>
 
