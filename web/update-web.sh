00ff5a980979ccc7a949427bdf79611d7a8db531
Francis Brosnan Blázquez <francis@aspl.es>
2007-07-30 17:07:09 +0000

    turbulence: * [fix] Commiting more updates to the web and fixed configure.ac options   to require axl 0.4.6 and vortex 1.0.5.

--- web/update-web.sh
+++ web/update-web.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-scp turbulence.css index.html downloads.html turbulence.html aspl@www.aspl.es:www/turbulence
-scp images/*.png images/*.gif images/*.jpg aspl@www.aspl.es:www/turbulence/images/
+read -p "Update html files (y/n): " html
+if [ $html == "y" ]; then
+	scp turbulence.css index.html install.html tunnel.html sasl.html extending.html doc.html news.html configuring.html downloads.html turbulence.html commercial.html licensing.html support.html aspl@www.aspl.es:www/turbulence
+fi
+
+read -p "Update image files (y/n): " images
+if [ $images == "y" ]; then
+	scp images/*.png images/*.gif images/*.jpg aspl@www.aspl.es:www/turbulence/images/
+fi
 
