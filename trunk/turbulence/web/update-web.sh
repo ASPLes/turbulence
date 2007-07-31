48cc9496ff5f24252c9d25a9fb15da48323e835a
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2007-07-31 17:10:18 +0000

    turbulence: * [fix] README, TODO and web site updates.

--- trunk/turbulence/web/update-web.sh
+++ trunk/turbulence/web/update-web.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+echo "Updating mod-test.c.html"
+(cd ../modules/mod-test; c2html mod-test.c; cp mod-test.c.html ../../web/)
+(cp ../modules/mod-test/mod-test.c .)
+(cp ../modules/mod-test/Makefile.am .)
+(cp ../modules/mod-test/mod-test.xml.in .)
+
 read -p "Update html files (y/n): " html
 if [ $html == "y" ]; then
 	scp turbulence.css index.html install.html tunnel.html sasl.html extending.html doc.html news.html configuring.html downloads.html turbulence.html commercial.html licensing.html support.html aspl@www.aspl.es:www/turbulence
