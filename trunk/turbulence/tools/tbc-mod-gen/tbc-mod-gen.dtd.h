918fa665f630673d03176b1a0ff468f7006b1575
acinom <acinom@9996078b-26e8-0310-a12f-ea49881d8364>
2008-08-01 15:41:59 +0000

    turbulence/tbc-mod-gen: * [new] Making the tool to include the DTD inside the code.

---
+++ trunk/turbulence/tools/tbc-mod-gen/tbc-mod-gen.dtd.h
@@ -0,0 +1,26 @@
+/**
+ * C inline representation for DTD tbc-mod-gen.dtd, created by axl-knife
+ */
+#ifndef __TBC_MOD_GEN_DTD_H__
+#define __TBC_MOD_GEN_DTD_H__
+#define TBC_MOD_GEN_DTD "\n\
+<!ELEMENT mod-def (name, description, source-code)>                 \
+                                                                    \
+<!ATTLIST mod-def sources CDATA #IMPLIED>                           \
+                                                                    \
+<!ELEMENT name (#PCDATA)>                                           \
+                                                                    \
+<!ELEMENT description (#PCDATA)>                                    \
+                                                                    \
+<!ELEMENT source-code (additional-content?, init, close, reconf)>   \
+                                                                    \
+<!ELEMENT additional-content (#PCDATA)>                             \
+                                                                    \
+<!ELEMENT init (#PCDATA)>                                           \
+                                                                    \
+<!ELEMENT close (#PCDATA)>                                          \
+                                                                    \
+<!ELEMENT reconf (#PCDATA)>                                         \
+                                                                    \
+\n"
+#endif
