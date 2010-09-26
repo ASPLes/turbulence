7cf1aa695a2a25bcb6989acd9a045ecccae70480
Francis Brosnan Blázquez <francis@aspl.es>
2010-09-26 14:44:28 +0000

    turbulence: * [fix] Adding additional debugging info to console signal handling.

--- src/turbulence-signal.c
+++ src/turbulence-signal.c
@@ -201,6 +201,7 @@ void turbulence_signal_exit (TurbulenceCtx * ctx, int _signal)
 		/* check current termination option */
 		doc  = turbulence_config_get (ctx);
 		node = axl_doc_get (doc, "/turbulence/global-settings/on-bad-signal");
+		error ("applyinng configured action %s", HAS_ATTR (node, "action") ? ATTR_VALUE (node, "action") : "not defined");
 		if (HAS_ATTR_VALUE (node, "action", "ignore")) {
 			/* do notify if enabled */
 			CHECK_AND_REPORT_MAIL_TO ("Bad signal received at turbulence process, default action: ignore",
