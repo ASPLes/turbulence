32f675bee29356a257f021a2893f60ef066e1c35
Francis Brosnan Blázquez <francis@aspl.es>
2011-03-27 20:58:09 +0000

    turbulence: * [new] Updated mod-sasl-mysql to allow configuring an SQL query   which is called to log auth operations (with ok and failure status).   Updated documentation and example to show how to use it.
    * [fix] Fixed memory leak at mod-sasl-mysql causing to leak axlError
      reference on query failure.

--- modules/mod-sasl/mysql.sasl.dtd.h
+++ modules/mod-sasl/mysql.sasl.dtd.h
@@ -4,25 +4,35 @@
 #ifndef __MYSQL_SASL_DTD_H__
 #define __MYSQL_SASL_DTD_H__
 #define MYSQL_SASL_DTD "\n\
-<!-- <sasl-auth-db> -->                                       \
-<!ELEMENT sasl-auth-db (connection-settings, get-password)>   \
-                                                              \
-<!-- <connection-settings> -->                                \
-<!ELEMENT connection-settings EMPTY>                          \
-<!ATTLIST connection-settings                                 \
-   user            CDATA   #REQUIRED                          \
-   password        CDATA   #REQUIRED                          \
-   database        CDATA   #REQUIRED                          \
-   host            CDATA   #REQUIRED                          \
-   port            CDATA   #IMPLIED>                          \
-                                                              \
-<!-- <get-password> -->                                       \
-<!ELEMENT get-password EMPTY>                                 \
-<!ATTLIST get-password                                        \
-   query           CDATA   #REQUIRED>                         \
-                                                              \
-                                                              \
-                                                              \
-                                                              \
+<!-- <sasl-auth-db> -->                                                              \
+<!ELEMENT sasl-auth-db (connection-settings, get-password, auth-log?, ip-filter?)>   \
+                                                                                     \
+<!-- <connection-settings> -->                                                       \
+<!ELEMENT connection-settings EMPTY>                                                 \
+<!ATTLIST connection-settings                                                        \
+   user            CDATA   #REQUIRED                                                 \
+   password        CDATA   #REQUIRED                                                 \
+   database        CDATA   #REQUIRED                                                 \
+   host            CDATA   #REQUIRED                                                 \
+   port            CDATA   #IMPLIED>                                                 \
+                                                                                     \
+<!-- <get-password> -->                                                              \
+<!ELEMENT get-password EMPTY>                                                        \
+<!ATTLIST get-password                                                               \
+   query           CDATA   #REQUIRED>                                                \
+                                                                                     \
+<!-- <auth-log> -->                                                                  \
+<!ELEMENT auth-log EMPTY>                                                            \
+<!ATTLIST auth-log                                                                   \
+   query           CDATA   #REQUIRED>                                                \
+                                                                                     \
+<!-- <ip-filter> -->                                                                 \
+<!ELEMENT ip-filter EMPTY>                                                           \
+<!ATTLIST ip-filter                                                                  \
+   query           CDATA   #REQUIRED>                                                \
+                                                                                     \
+                                                                                     \
+                                                                                     \
+                                                                                     \
 \n"
 #endif
