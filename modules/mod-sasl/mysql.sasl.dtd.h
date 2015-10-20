c3c6d6c44b565f1db4cb8371919e3a8b7e04b7d6
Francis Brosnan Blázquez <francis@aspl.es>
2015-10-20 15:12:42 +0000

    turbulence: * [fix] Updated mod-sasl-mysql extension library to allow   implementing alternative auth locations with <get-password-alt>   and <get-password-alt-cleanup>. This new feature is been used   by Core-Admin to provide auto login features

--- modules/mod-sasl/mysql.sasl.dtd.h
+++ modules/mod-sasl/mysql.sasl.dtd.h
@@ -4,35 +4,45 @@
 #ifndef __MYSQL_SASL_DTD_H__
 #define __MYSQL_SASL_DTD_H__
 #define MYSQL_SASL_DTD "\n\
-<!-- <sasl-auth-db> -->                                                              \
-<!ELEMENT sasl-auth-db (connection-settings, get-password, auth-log?, ip-filter?)>   \
-                                                                                     \
-<!-- <connection-settings> -->                                                       \
-<!ELEMENT connection-settings EMPTY>                                                 \
-<!ATTLIST connection-settings                                                        \
-   user            CDATA   #REQUIRED                                                 \
-   password        CDATA   #REQUIRED                                                 \
-   database        CDATA   #REQUIRED                                                 \
-   host            CDATA   #REQUIRED                                                 \
-   port            CDATA   #IMPLIED>                                                 \
-                                                                                     \
-<!-- <get-password> -->                                                              \
-<!ELEMENT get-password EMPTY>                                                        \
-<!ATTLIST get-password                                                               \
-   query           CDATA   #REQUIRED>                                                \
-                                                                                     \
-<!-- <auth-log> -->                                                                  \
-<!ELEMENT auth-log EMPTY>                                                            \
-<!ATTLIST auth-log                                                                   \
-   query           CDATA   #REQUIRED>                                                \
-                                                                                     \
-<!-- <ip-filter> -->                                                                 \
-<!ELEMENT ip-filter EMPTY>                                                           \
-<!ATTLIST ip-filter                                                                  \
-   query           CDATA   #REQUIRED>                                                \
-                                                                                     \
-                                                                                     \
-                                                                                     \
-                                                                                     \
+<!-- <sasl-auth-db> -->                                                                                                            \
+<!ELEMENT sasl-auth-db (connection-settings, get-password, get-password-alt?, get-password-alt-cleanup?, auth-log?, ip-filter?)>   \
+                                                                                                                                   \
+<!-- <connection-settings> -->                                                                                                     \
+<!ELEMENT connection-settings EMPTY>                                                                                               \
+<!ATTLIST connection-settings                                                                                                      \
+   user            CDATA   #REQUIRED                                                                                               \
+   password        CDATA   #REQUIRED                                                                                               \
+   database        CDATA   #REQUIRED                                                                                               \
+   host            CDATA   #REQUIRED                                                                                               \
+   port            CDATA   #IMPLIED>                                                                                               \
+                                                                                                                                   \
+<!-- <get-password> -->                                                                                                            \
+<!ELEMENT get-password EMPTY>                                                                                                      \
+<!ATTLIST get-password                                                                                                             \
+   query           CDATA   #REQUIRED>                                                                                              \
+                                                                                                                                   \
+<!-- <get-password-alt> -->                                                                                                        \
+<!ELEMENT get-password-alt EMPTY>                                                                                                  \
+<!ATTLIST get-password-alt                                                                                                         \
+   query           CDATA   #REQUIRED>                                                                                              \
+                                                                                                                                   \
+<!-- <get-password-alt-cleanup> -->                                                                                                \
+<!ELEMENT get-password-alt-cleanup EMPTY>                                                                                          \
+<!ATTLIST get-password-alt-cleanup                                                                                                 \
+   query           CDATA   #REQUIRED>                                                                                              \
+                                                                                                                                   \
+<!-- <auth-log> -->                                                                                                                \
+<!ELEMENT auth-log EMPTY>                                                                                                          \
+<!ATTLIST auth-log                                                                                                                 \
+   query           CDATA   #REQUIRED>                                                                                              \
+                                                                                                                                   \
+<!-- <ip-filter> -->                                                                                                               \
+<!ELEMENT ip-filter EMPTY>                                                                                                         \
+<!ATTLIST ip-filter                                                                                                                \
+   query           CDATA   #REQUIRED>                                                                                              \
+                                                                                                                                   \
+                                                                                                                                   \
+                                                                                                                                   \
+                                                                                                                                   \
 \n"
 #endif
