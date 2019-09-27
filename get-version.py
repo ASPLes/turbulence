bab910a697d280a95469773b0933c64a04586474
Francis Brosnan Blázquez <francis@aspl.es>
2019-09-27 16:06:01 +0200

    core-admin: * [fix] Updated get-version.py to update LATEST-VERSION and ChangeLog

--- get-version.py
+++ get-version.py
@@ -3,6 +3,15 @@
 from core_admin_common import command, support
 import sys
 
+import time
+start = time.time ()
+print "INFO: wait to avoid overwhelming github.."
+# implement a random wait to avoid Too many requests error from github.com
+from random import randint
+from time import sleep
+sleep (randint(1,20))
+print "INFO: wait done (%d seconds waited).." % (time.time () - start)
+
 (osname, oslongname, osversion) = support.get_os ()
 release_name = osversion.split (" ")[1]
 no_github_com_access = ["lenny", "squeeze"]
@@ -28,7 +37,8 @@ print "INFO: Updated vesion to: %s" % version
 open ("VERSION", "w").write ("%s\n" % version)
 open ("LATEST-VERSION", "w").write ("%s\n" % version)
 
-
+# also update Changelog
+command.run ("svn log > Changelog")
 
 
 
